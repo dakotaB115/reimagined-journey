3684ddfee7eb43c223442c02b641b96b21f9a7b6
Dakota Brown <dakotabrown@macbook-pro-25.local>
2018-10-17 10:36:38 -0500

    Initial Commit

---
+++ assessment 1234567/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assessment 1234567
+//
+//  Created by Dakota Brown on 10/17/18.
+//  Copyright © 2018 Dakota Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
